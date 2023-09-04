35fb1127f4ce36f09d5d27a3c7999575fce5c151
cidarosa <lucita.rosa@gmail.com>
2023-09-03 23:25:48 -0300

    Project created

---
+++ backend/src/main/java/com/acrdev/acrcatalog/AcrcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.acrdev.acrcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcrcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcrcatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/acrdev/acrcatalog/AcrcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acrdev.acrcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcrcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
