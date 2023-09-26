6de479405328600ee4efe9ce19ce05ad31d5ed2a
cidarosa <lucita.rosa@gmail.com>
2023-09-26 16:09:39 -0300

    Product insert, update, delete

--- backend/src/main/java/com/acrdev/acrcatalog/services/ProductService.java
+++ backend/src/main/java/com/acrdev/acrcatalog/services/ProductService.java
@@ -1,7 +1,10 @@
 package com.acrdev.acrcatalog.services;
 
+import com.acrdev.acrcatalog.dto.CategoryDTO;
 import com.acrdev.acrcatalog.dto.ProductDTO;
+import com.acrdev.acrcatalog.entities.Category;
 import com.acrdev.acrcatalog.entities.Product;
+import com.acrdev.acrcatalog.repositories.CategoryRepository;
 import com.acrdev.acrcatalog.repositories.ProductRepository;
 import com.acrdev.acrcatalog.services.exceptions.DatabaseException;
 import com.acrdev.acrcatalog.services.exceptions.ResourceNotFoundException;
@@ -19,6 +22,9 @@ public class ProductService {
 
     @Autowired
     private ProductRepository repository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Transactional(readOnly = true)
     public Page<ProductDTO> findAllPaged(PageRequest pageRequest){
@@ -39,16 +45,18 @@ public class ProductService {
     @Transactional
     public ProductDTO insert(ProductDTO dto){
         Product entity = new Product();
-
+        //método auxiliar
+        copyDtoToEntity(dto, entity);
+        
         entity = repository.save(entity);
         return new ProductDTO(entity);
     }
-
     @Transactional
     public ProductDTO update(Long id, ProductDTO dto){
         try{
             Product entity = repository.getReferenceById(id);
-
+            //método auxiliar
+            copyDtoToEntity(dto, entity);
             entity = repository.save(entity);
             return new ProductDTO(entity);
         } catch (EntityNotFoundException ex){
@@ -66,5 +74,20 @@ public class ProductService {
         } catch (DataIntegrityViolationException ex){
             throw new DatabaseException("Falha de integridade referencial");
         }
+    }
+
+    private void copyDtoToEntity(ProductDTO dto, Product entity) {
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgUrl(dto.getImgUrl());
+        entity.setDate(dto.getDate());
+
+        entity.getCategories().clear();
+        for(CategoryDTO catDto : dto.getCategories()){
+            Category category = categoryRepository.getReferenceById(catDto.getId());
+            entity.getCategories().add(category);
+        }
+
     }
 }
