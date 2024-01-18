295fba58e8b8da59f84d3a834fd778aed5673781
cidarosa <lucita.rosa@gmail.com>
2024-01-18 17:42:02 -0300

    primeiro passo da recuperação de senha

---
+++ backend/src/main/java/com/acrdev/acrcatalog/services/AuthService.java
@@ -0,0 +1,12 @@
+package com.acrdev.acrcatalog.services;
+
+import com.acrdev.acrcatalog.dto.EmailDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    public void createRecoverToken(EmailDTO body) {
+
+    }
+}
